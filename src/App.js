bd9b81e7a918c12506bf0e8a10aaab431bee8e61
vishaljadhav7 <vishalj.7661@gmail.com>
2024-04-26 17:58:12 +0530

    written logic for mutiple image slider

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 import Body from './Components/Body';
 import appStore from './utils/appStore'
 import { Provider } from 'react-redux';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 8,
+    slidesToScroll: 3
+  };
   return (
    <Provider store={appStore} >
      <Body/>
--- src/Components/MovieLists.js
+++ src/Components/MovieLists.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import MovieCard from './MovieCard'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const MovieLists = ({title, movies}) => {
     if(!movies) return ;
-    // console.log("from movielists  ", movies)
-  return (
-    <div className='px-6  text-white '>
-        <h1 className='text-lg md:text-2xl py-4'>{title} </h1>
-        <div className='flex overflow-x-scroll'>
-            <div className='flex gap-2'>
-            {movies.map(movie => <MovieCard poster_path={movie.poster_path} key={movie.id}/>)}
-        </div>
-      </div>
-       
-    </div>
-  )
-}
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 8,
+      slidesToScroll: 1
+    };
 
+    // console.log("from movielists  ", movies)
+    return (
+      <div className='px-6 text-white'>
+        <h1 className='text-lg md:text-2xl py-4'>{title}</h1>
+        <Slider {...settings}>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <MovieCard poster_path={movie.poster_path} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    );
+  };
 export default MovieLists
