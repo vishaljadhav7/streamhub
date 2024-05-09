e46981fd18aa8af17a1c18a346292a95daa6d709
vishaljadhav7 <vishalj.7661@gmail.com>
2024-05-09 18:44:59 +0530

    made changes in the carousel

--- src/Components/MovieLists.js
+++ src/Components/MovieLists.js
@@ -4,62 +4,19 @@ import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const MovieLists = ({title, movies}) => {
-    if(!movies) return ;
-
-    var settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 8,
-      slidesToScroll: 2,
-      initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false,
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    };
-
-    // console.log("from movielists  ", movies)
-    return (
-      <div className='px-6 text-white'>
-        <h1 className='text-lg md:text-2xl py-4'>{title}</h1>
-          {/* <div className='flex justify-center p-2 '>  </div>
-            <div className='flex overflow-x-scroll'> */}
-             <Slider {...settings} >
-               {movies.map((movie) => (
-                 <div key={movie.id} className='flex justify-around' >
-                   <MovieCard poster_path={movie.poster_path} />
-                 </div>
-                ))}
-             </Slider> 
-
-            {/* </div>
+const MovieLists = ({ movies, title }) => {
+  return (
+    <div className='px-6 text-white'>
+      <h1 className='text-lg md:text-2xl py-4'>{title}</h1>
+      <div className='flex overflow-x-scroll'>
+        {movies.map((movie) => (
+          <div key={movie.id} className='flex justify-around'>
+            <MovieCard poster_path={movie.poster_path} />
           </div>
-    */}
+        ))}
       </div>
-    );
-  };
+    </div>
+  );
+};
+
 export default MovieLists
