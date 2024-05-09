7cc3bc5fc6959b061989c99369234722933cc6f9
vishaljadhav7 <vishalj.7661@gmail.com>
2024-05-09 18:47:30 +0530

    data access change

--- src/Components/MovieLists.js
+++ src/Components/MovieLists.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import MovieCard from './MovieCard'
-import Slider from 'react-slick'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const MovieLists = ({ movies, title }) => {
+  if(!movies) return;
   return (
     <div className='px-6 text-white'>
       <h1 className='text-lg md:text-2xl py-4'>{title}</h1>
       <div className='flex overflow-x-scroll'>
-        {movies.map((movie) => (
+        {movies?.map((movie) => (
           <div key={movie.id} className='flex justify-around'>
             <MovieCard poster_path={movie.poster_path} />
           </div>
